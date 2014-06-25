5bed15b29d0271250ad961d5e133dfc4e664bbe9
Christian Moniatis <christian.moniatis@gmail.com>
2014-06-26 00:10:26 +0200

    Started to clean up the code

--- update.rb
+++ update.rb
@@ -1,7 +1,6 @@
-#require 'pry'
 
-unless ARGV[0] && ARGV[1]
-  puts "update_shore.rb <action> <folder>"
+unless %w{-i -u}.index(ARGV[0]) && ARGV[1]
+  puts "update_shore.rb <action> <folder> [playlist_name]"
   puts "-i  Initialize Folder"
   puts "-u  Update Folder"
   exit
@@ -9,32 +8,39 @@ end
 
 Dir.chdir ARGV[1]
 
-PLAYLIST = "1AAList.m3u8"
+PLAYLIST = ARGV[2] || "1AAList.m3u8"
 NUMBER_RX = /\-\-(\d+)\-\-/
+LIST = "http://www.youtube.com/watch?list=PLpr-NGsAGodEbDePSO3wivni39lgdLQjW"
 
-print "looking for last video ... "
-list = File.read PLAYLIST
+#Reading Playlist to enable continuing from the end of the list
 num = "nothing"
-list.each_line{|line| m = line.match(NUMBER_RX) ; num = m[1].to_i if m}
-puts "Found #{num}"
+if File.exists? PLAYLIST then
+  print "looking for last video ... "
+  list = File.read PLAYLIST
+  list.each_line do |line|
+    m = line.match(NUMBER_RX)
+    if m then
+      m = m[1].to_i
+      num = m if m > num.to_i
+    end
+  end
+  puts "Found #{num}"
+end
 
-list = "http://www.youtube.com/watch?list=PLpr-NGsAGodEbDePSO3wivni39lgdLQjW"
-puts "Updating Video Files"
-#youtube_log = `youtube-dl -f 18 #{list}`
 
 command = case ARGV[0]
-  when "-i" 
-    "youtube-dl -f 18 -o \"%(title)s--%(playlist_index)s--%(id)s.%(ext)s\"  #{list}"
-  when "-u" 
-    puts "No Playlist found" and exit if num == "nothing"
-    "youtube-dl -f 18 --playlist-start #{num} -o \"%(title)s--%(playlist_index)s--%(id)s.%(ext)s\"  #{list}"
-  else
-    exit
-  end
+          when "-i"
+            "youtube-dl -f 18 -o \"%(title)s--%(playlist_index)s--%(id)s.%(ext)s\"  #{LIST}"
+          when "-u"
+            puts "No Playlist found" and exit if num == "nothing"
+            "youtube-dl -f 18 --playlist-start #{num} -o \"%(title)s--%(playlist_index)s--%(id)s.%(ext)s\"  #{LIST}"
+          else
+            exit
+          end
 
 io = IO.popen command
 
-until io.eof? 
+until io.eof?
   puts io.readline
 end
 
@@ -46,7 +52,7 @@ files.map! do |f|
   if m = f.match(NUMBER_RX) then
     f = [m[1].to_i,f]
   else
-    f = [9999,f] 
+    f = [9999,f]
   end
   f
 end
